bbe09c52c15470232b2a105ca3da4bc52783c4b8
Michael Shuster <m.shuster241@gmail.com>
2024-03-13 00:13:56 +1300

    feat: final examples and corrections

--- 2634_Array_filter/array_filter.js
+++ 2634_Array_filter/array_filter.js
@@ -29,7 +29,51 @@ const filteredArray = (arr, fn) => {
 };
 
 var arr = [0, 10, 20, 30];
-const greaterThan10 = (n) => n > 10;
-filteredArray(arr, greaterThan10);
 
-console.log(arr);
+const greaterThan10 = (n) => n > 10;
+
+filteredArray(arr, greaterThan10);
+console.log("example 1 arr", arr);
+
+// had to create a new array for the results to be pushed into as the original array was being mutated during the while loop, this is safer
+const filteredArray2 = (arr, fn) => {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+
+var arr2 = [1, 2, 3];
+
+const firstIndex = (n, i) => {
+  return i === 1; // change this between 0 and 2 to return different values from the array, or any integer -1 for the length of the array
+};
+
+filteredArray2(arr2, firstIndex);
+console.log("example 2 arr", arr2);
+
+var arr3 = [-2, -1, 0, 1, 2];
+
+// this returns [-2,0,1,2] which is wrong as it should return [-1,0,1,2,3] but the filteredArray function is designed to filter elements based on a condition (fn(arr[from])), not to transform them so does what is expected by leetcode
+const plusOne = (n) => {
+  return n + 1;
+};
+
+filteredArray(arr3, plusOne);
+console.log("example 3 arr", arr3);
+
+// a proper way of doing this would be to rewrite the filteredArray function like so
+
+const filteredArray3 = (arr, fn) => {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = fn(arr[i]); // Apply transformation function to each element
+  }
+};
+
+var arr4 = [-2, -1, 0, 1, 2];
+
+filteredArray3(arr4, plusOne);
+console.log("example 3 arr but correct", arr4);
