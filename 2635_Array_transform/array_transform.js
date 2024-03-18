84b9c8d4c935a42dde58cdcab36464723cf42282
Michael Shuster <m.shuster241@gmail.com>
2024-03-18 15:26:13 +1300

    feat: example one and 2 with constraints

--- 2635_Array_transform/array_transform.js
+++ 2635_Array_transform/array_transform.js
@@ -11,11 +11,13 @@
 // Explanation:
 // const newArray = map(arr, plusone); // [2,3,4]
 // The function increases each value in the array by one.
+
 // Example 2:
 
 // Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
 // Output: [1,3,5]
 // Explanation: The function increases each value by the index it resides in.
+
 // Example 3:
 
 // Input: arr = [10,20,30], fn = function constant() { return 42; }
@@ -27,3 +29,52 @@
 // 0 <= arr.length <= 1000
 // -109 <= arr[i] <= 109
 // fn returns a number
+
+const initialArray = [1, 2, 3];
+
+const increaseByOne = (array) => {
+  if (array.length < 0 || array.length > 1000) {
+    throw new Error("Array length out of bounds");
+  }
+
+  const increasedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < -109 || array[i] > 109) {
+      throw new Error("Element value out of bounds");
+    }
+    increasedArray.push(array[i] + 1);
+  }
+  return increasedArray;
+};
+
+const result = increaseByOne(initialArray);
+if (Array.isArray(result) && result.every(Number.isInteger)) {
+  console.log(result);
+} else {
+  console.log(
+    "Function did not return an array of integers,  try again but better"
+  );
+}
+
+const increaseByI = (array) => {
+  if (array.length < 0 || array.length > 1000) {
+    throw new Error("Array length out of bounds");
+  }
+  const increasedArray2 = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < -109 || array[i] > 109) {
+      throw new Error("Element value out of bounds");
+    }
+    increasedArray2.push(array[i] + i);
+  }
+  return increasedArray2;
+};
+
+const result2 = increaseByI(initialArray);
+if (Array.isArray(result2) && result2.every(Number.isInteger)) {
+  console.log(result2);
+} else {
+  console.log(
+    "Function did not return an array of integers,  try again but better"
+  );
+}
